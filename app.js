c80492e43bd56082742394ebd7d93c49e4ee52e5
SAUMYA JAIN <132819769+S2103aumya@users.noreply.github.com>
2024-07-11 12:42:55 +0530

    app.js

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ const sessionOptions=({
     }
 });
 
-// app.get("/",(req,res)=>{
-//      res.send("/listings");
-// });
+app.get("/",(req,res)=>{
+      res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
