b5271100471627e73b6b3bed4ba8f1d7f964f67e
saumya <jainsaumya303@gmail.com>
2024-07-11 12:44:51 +0530

    first commit

--- app.js
+++ app.js
@@ -72,11 +72,7 @@ const sessionOptions=({
 });
 
 app.get("/",(req,res)=>{
-<<<<<<< HEAD
      res.redirect("/listings");
-=======
-      res.redirect("/listings");
->>>>>>> c80492e43bd56082742394ebd7d93c49e4ee52e5
 });
 
 app.use(session(sessionOptions));
